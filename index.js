0b658aa758bf687072995b487ff200e2da9a2a4e
James Halliday <mail@substack.net>
2013-11-08 12:44:54 +0800

    match the extra params

--- index.js
+++ index.js
@@ -5,15 +5,40 @@ var sub = require('level-sublevel');
 var db = sub(level(__dirname + '/latlon.db', { encoding: 'json' }));
 
 module.exports = function (query, cb) {
-    var parts = query.split(/[\.,\s\/]+/);
-    var place = parts[0];
+    var parts = query.toLowerCase().split(/[\.,\s\/]+/);
     
+    (function next (index) {
+        if (index === 0) return cb(null, [])
+        
+        var terms = parts.slice(0, index).join(' ');
+        find(terms, function (err, res) {
+            if (err) return cb(err);
+            if (res.length === 0) return next(index - 1);
+            
+            var extras = parts.slice(index);
+            var matching = res.filter(function (row) {
+                return extras.every(function (e) {
+                    var ue = e.toUpperCase();
+                    return row.country === ue
+                        || row.adminCode === ue
+                        || row.altCountry.toUpperCase() === ue
+                    ;
+                });
+            });
+            
+            if (matching.length) cb(null, matching)
+            else next(index - 1)
+        });
+    })(parts.length);
+};
+    
+function find (query, cb) {
     var pending = 0, ended = false;
-    var results = [];
+    var results = {};
     
     var qstream = db.createReadStream({
-        start: place,
-        end: place + '~',
+        start: query,
+        end: query + '~',
         limit: 25
     });
     qstream.on('error', cb);
@@ -21,22 +46,26 @@ module.exports = function (query, cb) {
     
     function write (r) {
         ++ pending;
-        db.get(r.key.split('!')[1], function (err, row) {
+        var key = r.key.split('!')[1];
+        db.get(key, function (err, row) {
             row.from = r.key.split('!')[0];
-            results.push(row);
+            results[key] = row;
             if (--pending === 0 && ended) done()
         })
     }
-
+    
     function end () {
         ended = true;
         if (pending === 0) done();
     }
-
+    
     function done () {
-        var sorted = results.sort(function (a, b) {
+        var rows = Object.keys(results).map(function (key) {
+            return results[key]
+        });
+        var sorted = rows.sort(function (a, b) {
             return a.population < b.population ? 1 : -1;
         });
         cb(null, sorted);
     }
-};
+}
--- test/places.js
+++ test/places.js
@@ -10,3 +10,13 @@ test('cities', function (t) {
         t.equal(rows[0].lon, -122.2708);
     });
 });
+
+test('US city with state', function (t) {
+    t.plan(3);
+    
+    find('oakland, ca', function (err, rows) {
+        t.equal(rows[0].adminCode, 'CA');
+        t.equal(rows[0].lat, 37.80437);
+        t.equal(rows[0].lon, -122.2708);
+    });
+});
